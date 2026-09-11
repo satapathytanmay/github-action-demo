efafaf38808843535d185262561dddc588ce9904
satapathytanmay <satapathyt8@gmail.com>
2026-09-11 19:36:40 +0530

    Initial commit - Spring Boot app

---
+++ github-action-demo/src/test/java/com/main/github_action_demo/GithubActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.github_action_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
